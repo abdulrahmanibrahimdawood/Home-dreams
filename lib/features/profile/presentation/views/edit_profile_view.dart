8ccfb78e2661a49da0b5a55e2ef5f62b971b0971
Abdulrahman Dawoud <kabdo2223@gmail.com>
2026-04-03 20:27:14 +0200

    fix context issue

--- lib/features/profile/presentation/views/edit_profile_view.dart
+++ lib/features/profile/presentation/views/edit_profile_view.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_dreams/core/services/get_it_services.dart';
 import 'package:home_dreams/core/widgets/custom_app_bar_inside.dart';
+import 'package:home_dreams/features/profile/domain/repos/update_user_data_repo.dart';
+import 'package:home_dreams/features/profile/presentation/manager/update_user_data_cubit/update_user_data_cubit.dart';
 import 'package:home_dreams/features/profile/presentation/views/widgets/edit_profile_view_bloc_consumer.dart';
 
 class EditProfileView extends StatelessWidget {
   const EditProfileView({super.key});
   static const routeName = 'edit-profile';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBarInside(
-        title: 'الملف الشخصي',
-        context,
-        showNotification: false,
-      ),
-      body: EditProfileViewBlocConsumer(),
+    return BlocProvider(
+      create: (context) =>
+          UpdateUserDataCubit(getIt<UpdateUserDataRepo>()),
+        child: Scaffold(
+          appBar: buildAppBarInside(
+            title: 'الملف الشخصي',
+            context,
+            showNotification: false,
+          ),
+          body: const EditProfileViewBlocConsumer(),
+        )
     );
   }
 }
--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -3,17 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_dreams/core/services/get_it_services.dart';
 import 'package:home_dreams/core/widgets/custom_app_bar_inside.dart';
 import 'package:home_dreams/features/checkout/domain/repo/images_repo.dart';
-import 'package:home_dreams/features/profile/presentation/manager/cubit/upload_image_cubit.dart';
-import 'package:home_dreams/features/profile/presentation/views/widgets/profile_view_body.dart';
+ import 'package:home_dreams/features/profile/presentation/manager/cubit/upload_image_cubit.dart';
+ import 'package:home_dreams/features/profile/presentation/views/widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
   static const routeName = 'profile';
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UploadImageCubit(getIt<ImagesRepo>()),
-
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => UploadImageCubit(getIt<ImagesRepo>())),
+       ],
       child: Scaffold(
         appBar: buildAppBarInside(
           context,
